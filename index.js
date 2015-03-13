3af1ad855afb8d824adaf1031e7a30e0fc00ccf7
Mikael Hallgren <hallgren.mikael@gmail.com>
2015-03-13 17:30:14 +0100

    Lint and JSformat

--- index.js
+++ index.js
@@ -2,132 +2,29 @@
  * lucene-parser
  * https://github.com/brentertz/scapegoat
  *
- * Copyright (c) 2014 Brent Ertz
+ * Copyright (c) 2015 Mikael Hallgren
  * Licensed under the MIT license.
  */
 var _ = require('lodash');
 
-// LuceneParser = function(sSearchTerm) {
-//     this.sOriginalSearchTerm = null;
-//     this.sFormattedSearchTerm = null;
-//     this.setSearchTerm( sSearchTerm );
-// };
-
-// LuceneParser.Patterns = {
-//     'HYPHEN': {
-//         'pattern': new RegExp( '.+-.+', 'g' ),
-//         'parse': function(sTerm) {
-//             return '"' + sTerm + '"';
-//         }
-//     },
-//     'PERIOD': {
-//         'pattern': new RegExp( '[.]', 'g' ),
-//         'parse': function(sTerm) {
-//             return '"' + sTerm + '"';
-//         }
-//     },
-//     'UNDERSCORE': {
-//         'pattern': new RegExp( '[_]', 'g' ),
-//         'parse': function(sTerm) {
-//             return '"' + sTerm + '"';
-//         }
-//     },
-//     'QUOTE': {
-//         'pattern': new RegExp( '"(.+?)"', 'g' ),
-//         'parse': function(sTerm) {
-//             return  sTerm;
-//         }
-//     },
-//     'LUCENE': {
-//         'pattern': new RegExp( '(.+):(.+)', 'g' ),
-//         'parse': function(sTerm) {
-//             var sRegexp = LuceneParser.Patterns['LUCENE']['pattern'];
-//             var sParsedTerm = sTerm.match( sRegexp );
-//             var sValue = sParsedTerm[1];
-//             var sKey = sParsedTerm[0];
-//             console.log(sParsedTerm)
-//             if(sValue.charAt(sValue.lenght) === '"' && sValue.charAt(0) === '"'){
-//                 return sKey + ':' + sValue + '';
-//             } else {
-//                 return sKey + ':"' + sValue + '"';
-//             }
-//         }
-//     },
-//     'PATH': {
-//         'pattern': new RegExp( '(.+):/(.+)' ),
-//         'parse': function(sTerm) {
-//             return '"' + sTerm + '"';
-//         }
-//     },
-//     'OR': {
-//         'pattern': new RegExp( 'OR' ),
-//         'parse': function(sTerm) {
-//             return 'OR';
-//         }
-//     },
-//     'AND': {
-//         'pattern': new RegExp( 'AND' ),
-//         'parse': function(sTerm) {
-//             return 'AND';
-//         }
-//     }
-// };
-
-// LuceneParser.prototype.setSearchTerm = function(sSearchTerm) {
-//     var sTerm = sSearchTerm;
-//     if (!sTerm) {
-//         sTerm = '*';
-//     } else if (_.trim( sTerm ).length == 0) {
-//         sTerm = '*';
-//     }
-//     this.sOriginalSearchTerm = sTerm;
-//     this._parse();
-// };
-
-// LuceneParser.prototype.getFormattedSearchTerm = function() {
-//     return _.trim(this.sFormattedSearchTerm);
-// };
-// LuceneParser.prototype.getOriginalSearchTerm = function() {
-//     return this.sOriginalSearchTerm;
-// };
-
-// LuceneParser.prototype._parse = function() {
-//     this.sFormattedSearchTerm = '';
-//     var rTerms = this.sOriginalSearchTerm.split( ' ' );
-//     var oMatch = null;
-//     _.forEach( rTerms, function(sTerm) {
-//         oMatch = null;
-//         _.forEach( LuceneParser.Patterns, function(sRegexp, sKey) {
-//             if (sTerm.match( sRegexp['pattern'] )) {
-//                 oMatch = sRegexp;
-//             }
-//         }, this );
-
-//         if (sTerm === '*') {
-//             this.sFormattedSearchTerm += sTerm;
-//         } else if (oMatch) {
-//             this.sFormattedSearchTerm += oMatch['parse']( sTerm ) + ' ';
-//         } else if(sTerm !== "") {
-//             this.sFormattedSearchTerm += sTerm + '* ';
-//         }
-//     }, this );
-// };
-
 LuceneParser = function(sSearchTerm) {
     this.sOriginalSearchTerm = null;
     this.sFormattedSearchTerm = null;
-    this.setSearchTerm( sSearchTerm );
+    this.setSearchTerm(sSearchTerm);
     this.oLuceneTemplateCache = {};
 };
+
 /*
-* The order of the patterns are important. The match by priority because the patterns may match on similar parts of a string.
-*/
+ * The order of the patterns are important. The match by priority because the patterns may match on similar parts of a string.
+ */
 LuceneParser.SanitizePatterns = {
     'LUCENE': {
-        'pattern': new RegExp( '(.+):(.+)' ), // ex. status:closed
+        'pattern': new RegExp('(.+):(.+)'), // ex. status:closed
         'parse': function(sTerm) {
-            var sRegexp = LuceneParser.SanitizePatterns['LUCENE']['pattern'];
-            var sParsedTerm = sTerm.match( sRegexp );
+            var sRegexp = LuceneParser.SanitizePatterns['LUCENE'][
+                'pattern'
+            ];
+            var sParsedTerm = sTerm.match(sRegexp);
             var sValue = sParsedTerm[2];
             var sKey = sParsedTerm[1];
             return sKey + ':"' + sValue + '"';
@@ -135,48 +32,48 @@ LuceneParser.SanitizePatterns = {
     },
     'SINGLE_SPECIAL_CHAR': {
         'pattern': new RegExp('[-]', 'g'), // ex. leave hyphen alone in: cvimport.local - disk
-        'parse': function (sTerm) {
+        'parse': function(sTerm) {
             return '"' + sTerm + '"';
         }
     },
     'HYPHEN': {
-        'pattern': new RegExp( '.+-.+', 'g' ), // ex. test-server
+        'pattern': new RegExp('.+-.+', 'g'), // ex. test-server
         'parse': function(sTerm) {
             return '"' + sTerm + '"';
         }
     },
     'PERIOD': {
-        'pattern': new RegExp( '[.]', 'g' ), // ex. 192.168.0.1, mail@mail.com
+        'pattern': new RegExp('[.]', 'g'), // ex. 192.168.0.1, mail@mail.com
         'parse': function(sTerm) {
             return '"' + sTerm + '"';
         }
     },
     'UNDERSCORE': {
-        'pattern': new RegExp( '[_]', 'g' ), // ex. test_server
+        'pattern': new RegExp('[_]', 'g'), // ex. test_server
         'parse': function(sTerm) {
             return '"' + sTerm + '"';
         }
     },
     'PATH': {
-        'pattern': new RegExp( '(.+):/' ), // ex. C:/Path, Http://Google.com
+        'pattern': new RegExp('(.+):/'), // ex. C:/Path, Http://Google.com
         'parse': function(sTerm) {
             return '"' + sTerm + '"';
         }
     },
     'OR': {
-        'pattern': new RegExp( 'OR' ),
+        'pattern': new RegExp('OR'),
         'parse': function(sTerm) {
             return 'OR';
         }
     },
     'AND': {
-        'pattern': new RegExp( 'AND' ),
+        'pattern': new RegExp('AND'),
         'parse': function(sTerm) {
             return 'AND';
         }
     },
     'TO': {
-        'pattern': new RegExp( 'TO' ),
+        'pattern': new RegExp('TO'),
         'parse': function(sTerm) {
             return 'TO';
         }
@@ -188,24 +85,24 @@ LuceneParser.prototype.setSearchTerm = function(sSearchTerm) {
     var sTerm = sSearchTerm;
     if (!sTerm) {
         sTerm = '*';
-    } else if (_.trim( sTerm ).length == 0) {
+    } else if (_.trim(sTerm).length == 0) {
         sTerm = '*';
     }
     this.sOriginalSearchTerm = sTerm;
     this.sFormattedSearchTerm = sTerm;
 
     /*
-    * Extract valid lucene-expressions from search-string.
-    * Because if they are recognized as valid there is no point running them
-    * trough the sanitizer. Replacing the expression with a template-string
-    * that is matched in the sanitizer and then pops the expression right back.
-    */
+     * Extract valid lucene-expressions from search-string.
+     * Because if they are recognized as valid there is no point running them
+     * trough the sanitizer. Replacing the expression with a template-string
+     * that is matched in the sanitizer and then pops the expression right back.
+     */
     this._luceneTemplateSearchTerm();
     /*
-    * Sanitizes the search-string to build valid lucene-expressions and format
-    * the query to the needs of elastic-search.
-    * The sanitizer patterns are in the LuceneParser.SanitizePatterns-enum
-    */
+     * Sanitizes the search-string to build valid lucene-expressions and format
+     * the query to the needs of elastic-search.
+     * The sanitizer patterns are in the LuceneParser.SanitizePatterns-enum
+     */
     this._sanitizer();
 };
 
@@ -218,8 +115,8 @@ LuceneParser.prototype.getOriginalSearchTerm = function() {
 
 LuceneParser.prototype._luceneTemplateSearchTerm = function() {
     /*
-    * The order of the patterns are important. The match by priority because the patterns may match on similar parts of a string.
-    */
+     * The order of the patterns are important. The match by priority because the patterns may match on similar parts of a string.
+     */
     var oPatterns = {
         'Brackets': /([a-zA-Z]+:)\[([^\]]+)]/g, // ex. created:[now-30d/d TO now+1d/d]
         'Lucene': /-*([\w\W]|[^\s]+):"(.*?)"/g, // ex. -symptomname:"ms serv provis"
@@ -228,25 +125,28 @@ LuceneParser.prototype._luceneTemplateSearchTerm = function() {
     var that = this;
     var nTemplateCount = 0;
 
-    _.forEach( oPatterns, function(rxPattern) {
-        this.sFormattedSearchTerm = this.sFormattedSearchTerm.replace( rxPattern, function(sTerm) {
-            that.oLuceneTemplateCache['_' + nTemplateCount] = sTerm;
-            return '#{{' + (nTemplateCount++) + '}}#';
-        } );
-    }, this );
+    _.forEach(oPatterns, function(rxPattern) {
+        this.sFormattedSearchTerm = this.sFormattedSearchTerm.replace(
+            rxPattern,
+            function(sTerm) {
+                that.oLuceneTemplateCache['_' + nTemplateCount] =
+                    sTerm;
+                return '#{{' + (nTemplateCount++) + '}}#';
+            });
+    }, this);
 };
 LuceneParser.prototype._sanitizer = function() {
     this.sSanitized = '';
-    var rTerms = this.sFormattedSearchTerm.split( ' ' );
+    var rTerms = this.sFormattedSearchTerm.split(' ');
     var bBracket = false;
     var oMatch = null;
 
-    _.forEach( rTerms, function(sTerm) {
+    _.forEach(rTerms, function(sTerm) {
         oMatch = null;
-        var rTemplateMatch = sTerm.match( /#{{(\d+)}}#/ ); // Look if template-string match is made.
+        var rTemplateMatch = sTerm.match(/#{{(\d+)}}#/); // Look if template-string match is made.
         for (var sKey in LuceneParser.SanitizePatterns) {
             var sRegexp = LuceneParser.SanitizePatterns[sKey];
-            if (sTerm.match( sRegexp['pattern'] )) {
+            if (sTerm.match(sRegexp['pattern'])) {
                 oMatch = sRegexp;
                 break;
             }
@@ -254,25 +154,26 @@ LuceneParser.prototype._sanitizer = function() {
         if (sTerm === '*') {
             this.sSanitized += sTerm;
         } else if (rTemplateMatch) { // if a match is made to the template-string, then restore lucene-expression form cache.
-            this.sSanitized += this.oLuceneTemplateCache['_' + rTemplateMatch[1]] + ' ';
+            this.sSanitized += this.oLuceneTemplateCache['_' +
+                rTemplateMatch[1]] + ' ';
         } else if (oMatch) {
-            this.sSanitized += oMatch['parse']( sTerm ) + ' ';
+            this.sSanitized += oMatch['parse'](sTerm) + ' ';
         } else if (sTerm !== '') {
             this.sSanitized += sTerm + '* ';
         }
-    }, this );
+    }, this);
 };
 
 var parser = new LuceneParser();
 
 module.exports = {
-  setSearchTerm: function(query) {
-    parser.setSearchTerm(query);
-  },
-  getFormattedSearchTerm: function() {
-    return parser.getFormattedSearchTerm();
-  },
-  getOriginalSearchTerm: function() {
-    return parser.getOriginalSearchTerm();
-  }
+    setSearchTerm: function(query) {
+        parser.setSearchTerm(query);
+    },
+    getFormattedSearchTerm: function() {
+        return parser.getFormattedSearchTerm();
+    },
+    getOriginalSearchTerm: function() {
+        return parser.getOriginalSearchTerm();
+    }
 };
