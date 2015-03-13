eacefb68e069b1460dd4cc09eff4ec421dafeca5
Mikael Hallgren <hallgren.mikael@gmail.com>
2015-03-13 11:31:15 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Scapegoat
+=========
+
+A minimal node module providing utility methods to `escape` and `unescape` HTML entities
+
+## Installation
+
+```shell
+  npm install scapegoat --save
+```
+
+## Usage
+
+```js
+  var scapegoat = require('scapegoat')
+      escape = scapegoat.escape,
+      unescape = scapegoat.unescape;
+
+  var html = '<h1>Hello World</h1>',
+      escaped = escape(html),
+      unescaped = unescape(escaped);
+
+  console.log('html', html, 'escaped', escaped, 'unescaped', unescaped);
+```
+
+## Tests
+
+```shell
+   npm test
+```
+
+## Contributing
+
+In lieu of a formal styleguide, take care to maintain the existing coding style.
+Add unit tests for any new or changed functionality. Lint and test your code.
+
+## Release History
+
+* 0.1.0 Initial release
---
+++ index.js
@@ -0,0 +1,122 @@
+/**
+ * lucene-parser
+ * https://github.com/brentertz/scapegoat
+ *
+ * Copyright (c) 2014 Brent Ertz
+ * Licensed under the MIT license.
+ */
+var _ = require('lodash');
+
+LuceneParser = function(sSearchTerm) {
+    this.sOriginalSearchTerm = null;
+    this.sFormattedSearchTerm = null;
+    this.setSearchTerm( sSearchTerm );
+};
+
+LuceneParser.Patterns = {
+    'HYPHEN': {
+        'pattern': new RegExp( '.+-.+', 'g' ),
+        'parse': function(sTerm) {
+            return '"' + sTerm + '"';
+        }
+    },
+    'PERIOD': {
+        'pattern': new RegExp( '[.]', 'g' ),
+        'parse': function(sTerm) {
+            return '"' + sTerm + '"';
+        }
+    },
+    'UNDERSCORE': {
+        'pattern': new RegExp( '[_]', 'g' ),
+        'parse': function(sTerm) {
+            return '"' + sTerm + '"';
+        }
+    },
+    'LUCENE': {
+        'pattern': new RegExp( '(.+):(.+)' ),
+        'parse': function(sTerm) {
+            var sRegexp = LuceneParser.Patterns['LUCENE']['pattern'];
+            var sParsedTerm = sTerm.match( sRegexp );
+            var sValue = sParsedTerm[2];
+            var sKey = sParsedTerm[1];
+
+            if(sValue.charAt(sValue.lenght) === '"' && sValue.charAt(0) === '"'){
+                return sKey + ':' + sValue + '';
+            } else {
+                return sKey + ':"' + sValue + '"';
+            }
+        }
+    },
+    'PATH': {
+        'pattern': new RegExp( '(.+):/(.+)' ),
+        'parse': function(sTerm) {
+            return '"' + sTerm + '"';
+        }
+    },
+    'OR': {
+        'pattern': new RegExp( 'OR' ),
+        'parse': function(sTerm) {
+            return 'OR';
+        }
+    },
+    'AND': {
+        'pattern': new RegExp( 'AND' ),
+        'parse': function(sTerm) {
+            return 'AND';
+        }
+    }
+};
+
+LuceneParser.prototype.setSearchTerm = function(sSearchTerm) {
+    var sTerm = sSearchTerm;
+    if (!sTerm) {
+        sTerm = '*';
+    } else if (_.trim( sTerm ).length == 0) {
+        sTerm = '*';
+    }
+    this.sOriginalSearchTerm = sTerm;
+    this._parse();
+};
+
+LuceneParser.prototype.getFormattedSearchTerm = function() {
+    return this.sFormattedSearchTerm;
+};
+LuceneParser.prototype.getOriginalSearchTerm = function() {
+    return this.sOriginalSearchTerm;
+};
+
+LuceneParser.prototype._parse = function() {
+    this.sFormattedSearchTerm = '';
+    var rTerms = this.sOriginalSearchTerm.split( ' ' );
+    var oMatch = null;
+    _.forEach( rTerms, function(sTerm) {
+        oMatch = null;
+        _.forEach( LuceneParser.Patterns, function(sRegexp, sKey) {
+            if (sTerm.match( sRegexp['pattern'] )) {
+                oMatch = sRegexp;
+            }
+        }, this );
+
+        if (sTerm === '*') {
+            this.sFormattedSearchTerm += sTerm;
+        } else if (oMatch) {
+            this.sFormattedSearchTerm += oMatch['parse']( sTerm ) + ' ';
+        } else if(sTerm !== "") {
+            this.sFormattedSearchTerm += sTerm + '* ';
+        }
+    }, this );
+    _.trim(this.sFormattedSearchTerm);
+};
+
+var parser = new LuceneParser();
+module.exports = {
+  setSearchTerm: function(query) {
+    parser.setSearchTerm(query);
+  },
+  getFormattedSearchTerm: function() {
+    return parser.getFormattedSearchTerm();
+  },
+  getOriginalSearchTerm: function() {
+    return parser.getOriginalSearchTerm();
+  }
+};
